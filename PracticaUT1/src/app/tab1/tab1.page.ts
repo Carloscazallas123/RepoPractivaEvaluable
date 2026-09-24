31b648162591cb6b33ece0ba8b47545e629fe714
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2026-09-24 12:13:43 +0200

    Refactor code structure for improved readability and maintainability

--- PracticaUT1/src/app/tab1/tab1.page.ts
+++ PracticaUT1/src/app/tab1/tab1.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard,IonCardHeader,IonCardTitle,IonCardSubtitle,IonCardContent } from '@ionic/angular';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard,IonCardHeader,IonCardTitle,IonCardSubtitle,IonCardContent, IonButton,IonImg } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonCard,IonCardHeader,IonCardTitle,IonCardSubtitle,IonCardContent ],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonCard,IonCardHeader,IonCardTitle,IonCardSubtitle,IonCardContent,IonButton,IonImg ],
 })
 export class Tab1Page {
   constructor() {}
